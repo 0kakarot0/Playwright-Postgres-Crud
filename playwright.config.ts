1be4c35d6399bf805131b9969f4a727d43252bcc
Ahtisham <ilyasahtsham@gmail.com>
2026-02-11 19:02:00 +0500

    Add Allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
     testDir: "./tests",
-    workers: 1, // DB tests are safer single-threaded
+    workers: 1, // recommended for DB tests
     globalSetup: require.resolve("./tests/global/global-setup"),
     globalTeardown: require.resolve("./tests/global/global-teardown"),
+
+    reporter: [
+        ["line"],
+        ["allure-playwright", { resultsDir: "allure-results", detail: true, suiteTitle: true }],
+    ],
 });
